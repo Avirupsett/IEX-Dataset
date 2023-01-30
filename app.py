577c585647c819e42b7bbfdc15108136063bba93
Avirup Sett <settavirup@gmail.com>
2023-01-30 22:23:34 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask,jsonify
+import pandas as pd
+import numpy as np
+import datetime
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/linechart', methods=['GET'])
+def hello_world():
+    df=pd.read_csv("Price_Volume_PurBid_SellBid_PurchaseVol_SellVol_For_Market_2 (1).csv")
+    df["TimeStamp_date"]=int(datetime.datetime.timestamp(df["BlockStartTime"]))
+    combine=df[["TimeStamp_date","MCPValues"]].values.tolist()
+    output = {'results': combine}
+
+    # return data
+    return jsonify(results=output)
